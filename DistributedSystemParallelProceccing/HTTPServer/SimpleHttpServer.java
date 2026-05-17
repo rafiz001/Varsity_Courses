ef222c796b9e564b242ed4550b389c7bfd2066ff
Md. Rafiz Uddin <rafiz001@protonmail.ch>
2026-05-17 20:03:41 +0600

    updated path

--- DistributedSystemParallelProceccing/HTTPServer/SimpleHttpServer.java
+++ DistributedSystemParallelProceccing/HTTPServer/SimpleHttpServer.java
@@ -12,26 +12,7 @@ class HelloHandler implements HttpHandler {
         exchange.getResponseHeaders().set("Content-Type", "text/plain");
         exchange.sendResponseHeaders(200, 0);
         OutputStream od = exchange.getResponseBody();
-        String response = "Hello, Rafiz.";
-        od.write(response.getBytes());
-        od.close();
-        exchange.close();
-        
-    }
-}
-class Html implements HttpHandler {
-
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-        exchange.getResponseHeaders().set("Content-Type", "text/html");
-        exchange.sendResponseHeaders(200, 0);
-        OutputStream od = exchange.getResponseBody();
-        String response = """
-        <html>
-        <body>rafiz <marquee>im rafiz</marquee>
-        </body>
-        </html>
-        """;
+        String response = "Hello, I'm Rafiz.";
         od.write(response.getBytes());
         od.close();
         exchange.close();
@@ -40,11 +21,9 @@ class Html implements HttpHandler {
 }
 
 public class SimpleHttpServer {
-
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/rafiz", new HelloHandler());
-        server.createContext("/", new Html());
 
         server.start();
         System.out.println("Server started and listening on port 8080");
